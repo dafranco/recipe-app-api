e4f3847268c7f6b611f7f6c7faefc2a1627d8fac
David Franco <dfranco@canarytechnologies.com>
2025-01-03 16:38:55 -0300

    Add recipe API

---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+"""
+Serializers for the Recipe API View.
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for the recipes."""
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
